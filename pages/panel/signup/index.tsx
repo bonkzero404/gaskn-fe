65609496a55ea5b960225b61d347bff8a0c256eb
Janitra Panji <bonkzero404@gmail.com>
2023-01-13 23:11:12 +0700

    Update redirect signup

--- pages/panel/signup/index.tsx
+++ pages/panel/signup/index.tsx
@@ -61,7 +61,7 @@ const SignUpPage = ({
 
     setDisabledWhileProccessButton(false);
 
-    router.push("/panel/activation");
+    setTimeout(() => router.push("/panel/activation"), 500);
 
     return false;
   };
