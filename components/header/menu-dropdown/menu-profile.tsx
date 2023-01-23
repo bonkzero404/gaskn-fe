3f1f1eb5eafc68538f969a97577fa4101073846a
Janitra Panji <bonkzero404@gmail.com>
2023-01-23 08:23:04 +0700

    Update menu profile

--- components/header/menu-dropdown/menu-profile.tsx
+++ components/header/menu-dropdown/menu-profile.tsx
@@ -23,6 +23,27 @@ export const MenuProfile = () => {
         Settings
       </Link>
 
+      <div className="max-[640px]:block sm:block md:hidden lg:hidden">
+        <DividerHorizontal />
+        <Link
+          href="/panel/profile"
+          className={
+            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent hover:bg-gray-100"
+          }
+        >
+          English
+        </Link>
+
+        <Link
+          href="/panel/settings"
+          className={
+            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent hover:bg-gray-100"
+          }
+        >
+          Indonesia
+        </Link>
+      </div>
+
       <DividerHorizontal />
       <Link
         href="/panel/signout"
